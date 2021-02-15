3212cbd091ecf06b7a2e00ab7adf8e1b015dfbe7
gustavohd18 <gustavo.hernandez@acad.pucrs.br>
2021-02-15 19:57:15 -0300

    add dowload gif

--- lib/pages/gif_page.dart
+++ lib/pages/gif_page.dart
@@ -1,9 +1,9 @@
 import 'package:find_gif/model/gif.dart';
 import 'package:flutter/material.dart';
+import 'package:image_downloader/image_downloader.dart';
 import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
-
   final GIF _gif;
   GifPage(this._gif);
 
@@ -12,21 +12,46 @@ class GifPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.white),
-        title: Text(_gif.title,
-          style: TextStyle(color: Colors.white)
-        ),
+        title: Text(_gif.title, style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.black,
         actions: [
           IconButton(
             icon: Icon(Icons.share),
-            onPressed: (){
+            onPressed: () {
+              ImageDownloader.downloadImage(_gif.source);
               Share.share(_gif.source);
             },
           )
         ],
       ),
-      body: Center(
-        child: Image.network(_gif.source),
+      body: Column(
+        children: [
+          Center(
+            child: Image.network(_gif.source),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(Icons.share),
+                color: Colors.white,
+                iconSize: 18,
+                onPressed: () {
+                  Share.share(_gif.source);
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.file_download),
+                color: Colors.white,
+                iconSize: 18,
+                onPressed: ()  {
+                  //for now dowload just work with android devices
+                   ImageDownloader.downloadImage(_gif.source);
+                },
+              ),
+            ],
+          )
+        ],
       ),
       backgroundColor: Colors.black,
     );
